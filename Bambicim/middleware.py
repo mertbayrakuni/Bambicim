a714ae61a44e9e9b8e47adf10f7b1b7eed561433
mertbayrakuni <mehmetmertbayrak@ogrenci.beykoz.edu.tr>
2025-08-17 15:28:02 +0300

    domain redirect

--- Bambicim/middleware.py
+++ Bambicim/middleware.py
@@ -1,14 +1,17 @@
+# Bambicim/middleware.py
 from django.http import HttpResponsePermanentRedirect
 
-CANONICAL_HOST = "bambicim.com"
 
 class CanonicalHostRedirectMiddleware:
+
     def __init__(self, get_response):
         self.get_response = get_response
+        self.canonical_host = "bambicim.com"
 
     def __call__(self, request):
         host = request.get_host().split(":")[0]
-        if host != CANONICAL_HOST:
-            url = f"https://{CANONICAL_HOST}{request.get_full_path()}"
-            return HttpResponsePermanentRedirect(url)
+        if host != self.canonical_host:
+            return HttpResponsePermanentRedirect(
+                f"https://{self.canonical_host}{request.get_full_path()}"
+            )
         return self.get_response(request)
