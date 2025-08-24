2c513f722388a8f81db1f77166163e6cb02af912
mertbayrakuni <mehmetmertbayrak@ogrenci.beykoz.edu.tr>
2025-08-24 10:56:25 +0300

    v1.4.4.1.2 bug fixes

--- core/views.py
+++ core/views.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail, BadHeaderError
+from django.db.models import F
 from django.http import JsonResponse, HttpResponseBadRequest
 from django.shortcuts import render, redirect
 from django.utils import timezone
